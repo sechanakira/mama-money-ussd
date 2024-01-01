2c4777130f0dc2f583967000e8e5515ce2a96a91
Shingi <sechanakira@gmail.com>
2024-01-01 12:34:58 +0200

    fix: typo

--- internal/db/migration/0001_create_ussd_session_table.up.sql
+++ internal/db/migration/0001_create_ussd_session_table.up.sql
@@ -3,7 +3,7 @@ CREATE TABLE ussd_session
     session_id            VARCHAR(255) PRIMARY KEY,
     msisdn                VARCHAR(255) NOT NULL,
     next_stage            VARCHAR(255),
-    country_ame           VARCHAR(255),
+    country_name           VARCHAR(255),
     amount                NUMERIC,
     foreign_currency_code VARCHAR(255),
     session_start_time    TIMESTAMP
